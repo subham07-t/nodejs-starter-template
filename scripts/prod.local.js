520a02b37f902b1bfa368321abc19494f28e8a78
subham07-t <82716446+subham07-t@users.noreply.github.com>
2024-02-10 18:14:13 +0530

    chore(prod.local.js): changes in script

--- scripts/prod.local.js
+++ scripts/prod.local.js
@@ -4,7 +4,7 @@ const majorNodeVersion = parseInt(process.versions.node.split('.')[0], 10);
 
 let command =
   majorNodeVersion >= 20
-    ? 'cross-env NODE_ENV=production node --env-file .env.production.local src/server.js'
-    : 'cross-env NODE_ENV=production node -r dotenv/config src/server.js dotenv_config_path=.env.production.local';
+    ? 'cross-env NODE_ENV=test node --env-file .env.production.local src/server.js'
+    : 'cross-env NODE_ENV=test node -r dotenv/config src/server.js dotenv_config_path=.env.production.local';
 
 execSync(command, { stdio: 'inherit' });
